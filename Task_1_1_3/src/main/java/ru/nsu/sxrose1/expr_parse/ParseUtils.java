99c38d83dcedf2216435c6d839af5c9b774f6a1f
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-10-10 20:44:32 +0700

    1-1-3 WIP again

--- Task_1_1_3/src/main/java/ru/nsu/sxrose1/expr_parse/ParseUtils.java
+++ Task_1_1_3/src/main/java/ru/nsu/sxrose1/expr_parse/ParseUtils.java
@@ -3,18 +3,45 @@ package ru.nsu.sxrose1.expr_parse;
 import ru.nsu.sxrose1.expr.Expression;
 import ru.nsu.sxrose1.expr.Number;
 
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class ParseUtils {
+  private record IntPair(int left, int right) {}
 
-  enum TokenType {
+  private enum TokenType {
     NUM,
     OP,
     BRACKET_OPEN,
     BRACKET_CLOSE
   }
 
-  record Token(String value, TokenType tokenType) {}
+  private enum OpType {
+    SUM,
+    SUB,
+    MUL,
+    DIV
+  }
+
+  private record Token(TokenType tokenType, double numValue, OpType opValue) {}
+
+  private static final Map<String, OpType> ops =
+      Map.of(
+          "+", OpType.SUM,
+          "-", OpType.SUB,
+          "*", OpType.MUL,
+          "/", OpType.DIV);
+
+  private static final Map<OpType, IntPair> bindingPowers =
+      Map.of(
+          OpType.SUM,
+          new IntPair(1, 2),
+          OpType.SUB,
+          new IntPair(1, 2),
+          OpType.MUL,
+          new IntPair(3, 4),
+          OpType.DIV,
+          new IntPair(3, 4));
 
   private static String tokenizeNumber(String buf) {
     int i = 0;
@@ -29,6 +56,7 @@ public class ParseUtils {
   }
 
   private static Stream<Token> tokenize(String exprStr) {
+
     Stream.Builder<Token> builder = Stream.builder();
 
     String buf = exprStr;
@@ -42,7 +70,7 @@ public class ParseUtils {
       if (Character.isDigit(buf.charAt(0))) {
         String num = tokenizeNumber(buf);
         buf = buf.substring(num.length());
-        builder.add(new Token(num, TokenType.NUM));
+        builder.add(new Token(TokenType.NUM, Double.parseDouble(num), null));
         continue;
       }
 
@@ -50,10 +78,12 @@ public class ParseUtils {
         buf = buf.substring(1);
         builder.add(
             new Token(
-                null, buf.charAt(0) == '(' ? TokenType.BRACKET_OPEN : TokenType.BRACKET_CLOSE));
+                buf.charAt(0) == '(' ? TokenType.BRACKET_OPEN : TokenType.BRACKET_CLOSE,
+                0.0,
+                null));
       }
 
-      builder.add(new Token(buf.substring(0, 0), TokenType.OP));
+      //      builder.add(new Token(Tok buf.substring(0, 0), TokenType.OP));
       buf = buf.substring(1);
     }
 
@@ -61,6 +91,10 @@ public class ParseUtils {
   }
 
   private static Number parseNumber(Stream<Token> stream) {}
+
+  private static Expression parseExprBP(Stream<Token> stream, int minBP) {}
+
+  private static Expression parseExpr(Stream<Token> stream) {}
 
   public static Expression parse(String exprStr) {}
 }
