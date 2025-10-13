cc0c6c6ef4ebd6585439ed9d0557db2703eb407b
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-10-13 15:10:27 +0700

    rm unused imports

--- Task_1_1_3/src/main/java/ru/nsu/sxrose1/Main.java
+++ Task_1_1_3/src/main/java/ru/nsu/sxrose1/Main.java
@@ -1,11 +1,6 @@
 package ru.nsu.sxrose1;
 
-import ru.nsu.sxrose1.expr.Expression;
-import ru.nsu.sxrose1.expr.eval.EvalUtils;
 import ru.nsu.sxrose1.expr.parse.ParseUtils;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
