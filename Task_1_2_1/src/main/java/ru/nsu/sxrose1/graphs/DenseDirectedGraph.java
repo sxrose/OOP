dc6c50cae35da0dc722d7fba7015338744ec20be
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-10-24 23:47:34 +0700

    shit

--- Task_1_2_1/src/main/java/ru/nsu/sxrose1/graphs/DenseDirectedGraph.java
+++ Task_1_2_1/src/main/java/ru/nsu/sxrose1/graphs/DenseDirectedGraph.java
@@ -1,32 +1,60 @@
 package ru.nsu.sxrose1.graphs;
 
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 
 public final class DenseDirectedGraph<T> implements DirectedGraph<T> {
+  private T[] verticesMap;
+  private boolean[][] adjMatrix;
 
-
+  DenseDirectedGraph(T[] elements) {
+    verticesMap = (T[]) Arrays.stream(elements).distinct().toArray();
+    adjMatrix = new boolean[verticesMap.length][verticesMap.length];
+  }
 
   /** {@inheritDoc} */
   @Override
   public Set<T> vertices() {
-    return Set.of();
+    return Set.of(verticesMap);
   }
 
+  /** {@inheritDoc} */
   @Override
   public Set<DirectedGraphEdge<T>> edges() {
-    return Set.of();
+    Set<DirectedGraphEdge<T>> res = new HashSet<>(Set.of());
+    for (int i = 0; i < adjMatrix.length; i++) {
+      for (int j = 0; j < adjMatrix.length; j++) {
+        if (adjMatrix[i][j]) res.add(new DirectedGraphEdge<>(verticesMap[i], verticesMap[j]));
+      }
+    }
+
+    return res;
   }
 
+  /** {@inheritDoc} */
   @Override
   public Optional<DirectedGraph<T>> addVertex(T elem) {
-    return Optional.empty();
+    for (var e : verticesMap) {
+      if (elem.equals(e)) return Optional.empty();
+    }
+
+    for (int i = 0; i < adjMatrix.length; i++)
+      adjMatrix[i] = Arrays.copyOf(adjMatrix[i], adjMatrix.length + 1);
+    adjMatrix = Arrays.copyOf(adjMatrix, adjMatrix.length + 1);
+
+    return Optional.of(this);
   }
 
+  /** {@inheritDoc} */
   @Override
   public Optional<DirectedGraph<T>> deleteVertex(T elem) {
+    int i = 0;
+    for (; i < verticesMap.length; i++) {
+      if (verticesMap[i].equals(elem)) break;
+    }
+
+    if (i == verticesMap.length) return Optional.empty();
+
     return Optional.empty();
   }
 
