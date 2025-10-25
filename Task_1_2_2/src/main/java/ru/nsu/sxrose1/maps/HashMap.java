63062fb39eb4d35dac34d29d3a7ce4af191086bd
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-10-25 18:16:41 +0700

    WIP

--- Task_1_2_2/src/main/java/ru/nsu/sxrose1/maps/HashMap.java
+++ Task_1_2_2/src/main/java/ru/nsu/sxrose1/maps/HashMap.java
@@ -1,8 +1,14 @@
 package ru.nsu.sxrose1.maps;
 
+import ru.nsu.sxrose1.utils.PrimesUtils;
+
+import java.util.Objects;
 import java.util.Optional;
 
 public class HashMap<K, E> implements Map<K, E> {
+  static final int MIN_CAPACITY = 31;
+  static final double MIN_LOAD_FACTOR = 0.4;
+
   private enum SlotTag {
     OCCUPIED,
     EMPTY,
@@ -11,23 +17,128 @@ public class HashMap<K, E> implements Map<K, E> {
 
   private record Slot(SlotTag tag, Object key, Object element) {}
 
-  HashMap() {}
+  private Slot[] data;
+  private int load = 0;
+  private int deleted = 0;
+
+  private int capacity() {
+    return data.length;
+  }
+
+  private double loadFactor() {
+    return ((double) load - deleted) / capacity();
+  }
+
+  private double effectiveLoadFactor() {
+    return ((double) load) / capacity();
+  }
+
+  private int index(Object key) {
+    return Integer.signum(key.hashCode()) % capacity();
+  }
+
+  private int probe(int ind) {
+    while (true) {
+      if (Objects.requireNonNull(data[ind].tag) == SlotTag.EMPTY) {
+        return ind;
+      }
+      ind = nextProbe(ind);
+    }
+  }
+
+  private int nextProbe(int ind) {
+    return (ind + 1) % capacity();
+  }
+
+  private Optional<Integer> findInd(Object key) {
+    assert (Objects.nonNull(key));
+    int ind = index(key);
+    while (true) {
+      switch (data[ind].tag) {
+        case EMPTY -> {
+          return Optional.empty();
+        }
+        case OCCUPIED -> {
+          if (Objects.requireNonNull(data[ind].key).equals(key)) return Optional.of(ind);
+        }
+      }
+
+      ind = nextProbe(ind);
+    }
+  }
+
+  private static int updatedCapacity(int newCap) {
+    return Optional.of(newCap)
+        .filter(PrimesUtils::exceed1000Prime)
+        .flatMap(PrimesUtils::getNextPrimeUnder1000)
+        .orElse(newCap);
+  }
+
+  private Map<K, E> insertNoResize(Object key, Object element) {
+    assert (load < capacity());
+    load += 1;
+    data[probe(index(key))] = new Slot(SlotTag.OCCUPIED, key, element);
+    return this;
+  }
+
+  private void rehash(int newCap) {
+    Slot[] oldData = data;
+    data = new Slot[newCap];
+    load = 0;
+    deleted = 0;
+
+    for (var s : oldData) {
+      if (s.tag == SlotTag.OCCUPIED)
+        insertNoResize(Objects.requireNonNull(s.key), Objects.requireNonNull(s.element));
+    }
+  }
+
+  private Optional<Map<K, E>> deleteNoResize(Object key) {
+    return findInd(key)
+        .map(
+            (ind) -> {
+              data[ind] = new Slot(SlotTag.DELETED, null, null);
+              deleted += 1;
+              return this;
+            });
+  }
+
+  /** Creates empty HashMap. */
+  HashMap() {
+    this(MIN_CAPACITY);
+  }
+
+  /**
+   * Creates empty HashMap with provided initial capacity.
+   *
+   * @param capacity initial capacity.
+   */
+  HashMap(int capacity) {
+    data = new Slot[capacity];
+    for (int i = 0; i < capacity; i++) data[i] = new Slot(SlotTag.EMPTY, null, null);
+  }
 
   /** {@inheritDoc} */
   @Override
   public Map<K, E> insert(K key, E element) {
-    return null;
+    if (effectiveLoadFactor() >= 1.0) rehash(updatedCapacity(2 * capacity() + 1));
+    return insertNoResize(key, element);
   }
 
   /** {@inheritDoc} */
   @Override
   public Optional<E> find(K key) {
-    return Optional.empty();
+    // we know that we put only correct slots
+    @SuppressWarnings("unchecked")
+    var res = findInd(key).map((ind) -> (E) Objects.requireNonNull(data[ind].element));
+    return res;
   }
 
   /** {@inheritDoc} */
   @Override
   public Optional<Map<K, E>> delete(K key) {
-    return Optional.empty();
+    if (loadFactor() <= MIN_LOAD_FACTOR)
+      rehash(updatedCapacity(Integer.max(MIN_CAPACITY, (int) (capacity() * MIN_LOAD_FACTOR))));
+    return deleteNoResize(key);
   }
 }
---
+++ Task_1_2_2/src/test/java/HashMapTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HashMapTest {
+  @Test
+  void integerTest() {
+    final Integer[] lows = {1, 2, 3, 42, 69, 127};
+    final Integer[] highs = {128, 256, 997, Integer.MAX_VALUE};
+    final Integer[] negatives = {-1, -42, Integer.MIN_VALUE};
+  }
+}
